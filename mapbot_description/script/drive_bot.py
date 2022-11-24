09b60fee5768fd1eb5a92bdf53f472daa7c170c9
bupeshr <bupeshkam@gmail.com>
2022-11-24 07:36:29 +0530

    created py and cpp for with move base client

---
+++ mapbot_description/script/drive_bot.py
@@ -0,0 +1,35 @@
+#! /usr/bin/python
+
+import rospy
+import actionlib
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+
+
+def movebase_client():
+    client = actionlib.SimpleActionClient('move_basse'. MoveBaseAction)
+    client.wait_fot_server()
+
+    goal = MoveBaseGoal()
+    goal.target_pose.header.frame_id = 'map'
+    goal.target_pose.header.stamp = rospy.Time.now()
+    goal.target_pose.pose.position.x = 0.5
+    goal.target_pose.pose.orientation.w = 1.0
+
+    client.send_goal(goal)
+    wait =client.wait_for_result()
+
+    if not wait:
+        rospy.logerr('Action server not availab')
+        rospy.signal_shutdown("podu ambutu then")
+    else:
+        return client.get_result()
+
+
+if __name__ == '__main__' :
+    try:
+        rospy.init_node('drive_bot')
+        result = movebase_client()
+        if result:
+            rospy.loginfo("goal execution done!!")
+    except rospy.ROSInterruptException:
+        rospy.loginfo("done ehhhhhhh")
